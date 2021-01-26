7a8d8146be768b28d0ceceb1fab22487299078d0
ttoppanda <coolpanda1219@outlook.com>
2021-01-26 18:17:58 +0530

    Backend api built

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import manage_items, manage_item, get_stocks
+from .views import manage_items, manage_item, get_stocks, get_stock_by_prefix
 
 urlpatterns = {
-    path("", manage_items, name="items"),
-    path("<slug:key>", manage_item, name="single_item"),
     path("stocks/", get_stocks, name="stocks"),
+    path("stocks/<slug:name>/", get_stock_by_prefix, name="stocks_by_name")
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- app/api/views.py
+++ app/api/views.py
@@ -13,65 +13,6 @@ redis_instance = redis.StrictRedis(
     host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=1, decode_responses=True
 )
 
-
-@api_view(["GET", "POST"])
-def manage_items(request, *args, **kwargs):
-    if request.method == "GET":
-        items = {}
-        count = 0
-        for key in redis_instance.keys("*"):
-            items[key.decode("utf-8")] = redis_instance.hget(key)
-            count += 1
-        response = {"count": count, "msg": f"Found {count} items.", "items": items}
-        return Response(response, status=200)
-    elif request.method == "POST":
-        item = json.loads(request.body)
-        key = list(item.keys())[0]
-        value = item[key]
-        redis_instance.set(key, value)
-        response = {"msg": f"{key} successfully set to {value}"}
-        return Response(response, 201)
-
-
-@api_view(["GET", "PUT", "DELETE"])
-def manage_item(request, *args, **kwargs):
-    if request.method == "GET":
-        if kwargs["key"]:
-            value = redis_instance.get(kwargs["key"])
-            if value:
-                response = {"key": kwargs["key"], "value": value, "msg": "success"}
-                return Response(response, status=200)
-            else:
-                response = {"key": kwargs["key"], "value": None, "msg": "Not found"}
-                return Response(response, status=404)
-    elif request.method == "PUT":
-        if kwargs["key"]:
-            request_data = json.loads(request.body)
-            new_value = request_data["new_value"]
-            value = redis_instance.get(kwargs["key"])
-            if value:
-                redis_instance.set(kwargs["key"], new_value)
-                response = {
-                    "key": kwargs["key"],
-                    "value": value,
-                    "msg": f"Successfully updated {kwargs['key']}",
-                }
-                return Response(response, status=200)
-            else:
-                response = {"key": kwargs["key"], "value": None, "msg": "Not found"}
-                return Response(response, status=404)
-
-    elif request.method == "DELETE":
-        if kwargs["key"]:
-            result = redis_instance.delete(kwargs["key"])
-            if result == 1:
-                response = {"msg": f"{kwargs['key']} successfully deleted"}
-                return Response(response, status=404)
-            else:
-                response = {"key": kwargs["key"], "value": None, "msg": "Not found"}
-                return Response(response, status=404)
-
-
 @api_view(["GET"])
 def get_stocks(request, *args, **kwargs):
     if request.method == "GET":
@@ -82,3 +23,22 @@ def get_stocks(request, *args, **kwargs):
             count += 1
         response = {"count": count, "msg": f"Found {count} items.", "items": items}
         return Response(response, status=200)
+
+
+@api_view(["GET"])
+def get_stock_by_prefix(request, *args, **kwargs):
+    if request.method == "GET":
+        if kwargs["name"]:
+            items = {}
+            count = 0
+            prefix = kwargs["name"].upper()
+            match = prefix + "*"
+            for key in redis_instance.scan_iter(match=match):
+                items[key] = redis_instance.hgetall(key)
+                count += 1
+            if len(items) == 0:
+                response = {"key": kwargs["key"], "value": None, "msg": "Not found"}
+                return Response(response, status=404)
+            else:
+                response = {"count": count, "msg": f"Found {count} items.", "items": items}
+                return Response(response, status=200)
